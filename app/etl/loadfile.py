495a29eac0f65fe1ef3e41ddc3662732e8a9b843
Gileno Duarte <gileno.cr.duarte@gmail.com>
2024-05-29 23:36:32 -0300

    fix:ajustado funcao de criacao da tabela

--- app/etl/loadfile.py
+++ app/etl/loadfile.py
@@ -1,42 +1,40 @@
-import pandas as pd
-from db import conection
 from unidecode import unidecode
+import re
+"""
+    funcao para mapear cabecalhos do arquivo explitado utilizando fatiamento
 
-normatize= lambda f: unicode(f.strip().replace(' ', '_').lower())
+"""
+
+normatize= lambda f: unidecode(f.strip().replace(' ', '_').lower())
+
 def map_fields(fields):
-    coluns=[(normatize(fields[1:4]), "VARCHAR(255)"),
-            (normatize(fields[19:28]), "VARCHAR(255)"),
-            (normatize(fields[29:42]), "VARCHAR(255)"),
-            (normatize(fields[43:65]), "VARCHAR(255)"),
-            (normatize(fields[66:80]), "VARCHAR(255)"),
-            (normatize(fields[81:111]), "VARCHAR(255)"),
-            (normatize(fields[112:131]), "VARCHAR(255)"),
-            (normatize(fields[132:154]), "VARCHAR(255)")]
-
+    coluns=[normatize(fields[1:4]),
+            normatize(fields[19:28]),
+            normatize(fields[29:42]),
+            normatize(fields[43:65]),
+            normatize(fields[66:80]),
+            normatize(fields[81:111]),
+            normatize(fields[112:131]),
+            normatize(fields[132:154])]
+   
+    #c=",".join([f"{colun[0],  colun[1]}".replace(',', '') for colun in coluns]).replace("('", '').replace("')",'').replace("'",'')
+    #c=sinitize(c)
+    #print(c)
+    
     return coluns
 
 
-
-file_path = "../uploads/Base.txt"
+file_path = "./uploads/Base.txt"
 file_lines=''
 coluns=''
 
-def create_base(file_path):
+
+def create_base(file_path, conection):
     with open(file_path, "r") as arquivo:
         head_line = arquivo.readline()
-        coluns=map_fields(head_line)
+        conection.create(map_fields(head_line))
+
         
-        sql= f"""
-            DROP TABLE IF EXISTS vendas;
-            CREATE TABLE vendas(
-                {",".join([f"{colun[0], colun[1]}" for colun in coluns])}
-            );"""
-        try:
-            con=conection.connect()
-            con.execute(sql)
-        except Exception as e:
-            print("Error in table creation: ", e)
-        con.close()
 
 
 def load():
@@ -59,3 +57,4 @@ print(cpf)
 #test=str(column_names).split('\t')
 #print(test)
 '''
+
