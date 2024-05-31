31b220a53dd122a1a341132cfdaa7d3067288b34
Gileno Duarte <gileno.cr.duarte@gmail.com>
2024-05-31 19:46:19 -0300

    fix: corrige funcao de carregamento dos dados

--- app/etl/loadfile.py
+++ app/etl/loadfile.py
@@ -10,59 +10,35 @@ class Vendas(Base):
     cpf= Column(String)
     private = Column(String)
     incompleto = Column(String)
+    data_ultima_compra= Column(String)
     ticket_medio = Column(String)
     ticket_medio_ultima_compra= Column(String)
     loja_mais_frequente= Column(String)
     loja_da_ultima_compra= Column(String)
     
-
-
-    """
-        funcao para mapear cabecalhos do arquivo explitado utilizando fatiamento
-
-    """
-    normatize= lambda f: unidecode(f.strip().replace(' ', '_').lower())
-
-
     def remove_empty_fields(self,data):
         return str(re.sub(",,{1,}",",",(str(data).replace(',','.').replace(' ',',')))).split(',')
 
-
-    '''def normatize_data(data):
-        data_list=[]
-        for d in data:
-            data_list.append(d.split(' '))
-        
-        for list in data_list:
-            for l in list:
-                if l=='':
-                    data_list
-        data_list=list(filter(is_not_empty_custom, data_list))
-        print(data_list[0])'''
-
     def recreate_table(self,tabela, engine, conection):
-        tabela.drop(engine, checkfirst=True)  # Remove a tabela se ela existir
+        tabela.drop(engine, checkfirst=True) 
         tabela.create(engine)
 
     def load(self, file_path, conection):
         with open(file_path, "r") as file:
             lines= file.readlines()
-            list_lines=[]
+            count=0
             for l in lines:
-                #list_lines.append(l)
-                print(self.remove_empty_fields(l))
-                conection.save(self.remove_empty_fields(l), self.__tablename__)
+                count+=1
+                if count==1:
+                    continue
+                fields=self.remove_empty_fields(l)
+                new_entry=Vendas(cpf=fields[0],private=fields[1],
+                                incompleto=fields[2],data_ultima_compra=fields[3],
+                                ticket_medio=fields[4], ticket_medio_ultima_compra=fields[5],
+                                loja_mais_frequente=fields[6],
+                                loja_da_ultima_compra=fields[7] )
+
+                conection.save(new_entry, self.__tablename__)
             
-#print(list_lines[1])
-        #print(remove_empty_fields([list_lines[1]]))
-        #conection.save(remove_empty_fields(list_lines), 'vendas')
-        
-        #for line in lines[1:]:
-        #    print(line)
-        #    break
-        #print(no_empty_fields_list[1])
-
-#load(file_path)
-
 
 
