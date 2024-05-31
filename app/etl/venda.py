283ad56a04179c45b01af34418e109e2d6fc86ed
Gileno Duarte <gileno.cr.duarte@gmail.com>
2024-05-31 20:59:23 -0300

    feat: adicionado metodo para validacao de cpfs e cnpjs invalidos e alteração dos tipos dos campos

--- app/etl/venda.py
+++ app/etl/venda.py
@@ -1,41 +1,49 @@
 from unidecode import unidecode
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Date, DECIMAL
 import re
 Base = declarative_base()
 
-formats_cpf= lambda cpf : f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+formats_cpf= lambda cpf : f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}" 
+formats_cnpj = lambda cnpj : f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:]}"
 class Vendas(Base):
     __tablename__ = 'vendas'
     id = Column(Integer, primary_key=True)
     cpf= Column(String)
-    private = Column(String)
-    incompleto = Column(String)
-    data_ultima_compra= Column(String)
-    ticket_medio = Column(String)
-    ticket_medio_ultima_compra= Column(String)
+    private = Column(Integer)
+    incompleto = Column(Integer)
+    data_ultima_compra= Column(Date, nullable=True)
+    ticket_medio = Column(DECIMAL(10, 2))
+    ticket_medio_ultima_compra= Column(DECIMAL(10, 2))
     loja_mais_frequente= Column(String)
     loja_da_ultima_compra= Column(String)
     cpf_valido= Column(Boolean, default=True)
+    cnpj_invalido= Column(Boolean, default=True)
     
-    def remove_empty_fields(self,data):
+    
+    @staticmethod
+    def remove_empty_fields(data):
         return str(re.sub(",,{1,}",",",(str(data).replace(',','.').replace(' ',',')))).split(',')
 
-    def recreate_table(self,table, engine, conection):
-        table.drop(engine, checkfirst=True) 
-        table.create(engine)
    
     @staticmethod
-    def cpf_is_valid(cpf):
-        cpf = re.sub(r'\D', '', cpf)
-        if cpf == cpf[0] * len(cpf):
-            return False, formats_cpf(cpf)
-        
-        elif len(cpf)==11:
-            return True, formats_cpf(cpf)
-        
-        else:
-            return False, formats_cpf(cpf)
+    def cpf_is_valid(cpfcnpj, t='f'):
+        cpfcnpj = re.sub(r'\D', '', cpfcnpj)
+        if t=='f':
+            if cpfcnpj == cpfcnpj[0] * len(cpfcnpj):
+                return False, formats_cpf(cpfcnpj)
+            elif len(cpfcnpj)==11:
+                return True, formats_cpf(cpfcnpj) 
+            else:
+                return False, formats_cpf(cpfcnpj)
+        elif t=='j':
+            if cpfcnpj == cpfcnpj[0] * len(cpfcnpj):
+                return False, formats_cnpj(cpfcnpj)
+            elif len(cpfcnpj) == 14:
+                return False, formats_cnpj(cpfcnpj)
+            else:
+                return False, formats_cpf(cpfcnpj)
+
 
         
     def load(self, file_path, conection):
