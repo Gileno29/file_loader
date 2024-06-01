cf2646cdb16c7bbedb40acf83e1e55c11afdd02e
Gileno Cordeiro Duarte <38367700+Gileno29@users.noreply.github.com>
2024-06-01 11:47:33 -0300

    fix: corrige problemas para upload de arquivos ao utilizar o gunicorn

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER='uploads/'
+UPLOAD_FOLDER=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = {'txt'}
@@ -72,14 +72,26 @@ def hello_world():
 '''  
 
 
-@app.route("/upload", methods=['POST', 'GET'])
+@app.route("/upload", methods=['POST'])
 def upload():
+
+    if request.method == 'POST':
+        print("o metodo é o post")
+        if 'file' not in request.files:
+            return 'No file part', 400
+        
+        
     file = request.files['file']
     if file:
       filename = secure_filename(file.filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      try:
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      except Exception as e:
+          print("Erro ao salvar", e)
       new_conection=conection.Conection()
       new_venda= venda.Vendas()
       new_conection.drop_table(new_venda)
       new_venda.load(str(os.path.join(app.config['UPLOAD_FOLDER'], filename)), new_conection)
+
+
 
