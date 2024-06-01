cee570efd57854e7ec99b43d3eb2c97087380659
Gileno Duarte <gileno.cr.duarte@gmail.com>
2024-06-01 14:58:26 -0300

    feat: adiciona templates separados para aplicação

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,  request, redirect, url_for, flash
+from flask import Flask,  request, render_template, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 from app.etl import venda
 from app.db import conection
@@ -16,60 +16,7 @@ def allowed_file(filename):
 
 @app.route("/")
 def hello_world():
-      return '''
-   <!doctype html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload de Arquivo</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            background-color: #f4f4f9;
-        }
-        .upload-container {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-        h1 {
-            color: #333;
-        }
-        input[type="file"] {
-            margin: 20px 0;
-        }
-        input[type="submit"] {
-            background-color: #4CAF50;
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-        input[type="submit"]:hover {
-            background-color: #45a049;
-        }
-    </style>
-</head>
-<body>
-    <div class="upload-container">
-        <h1>Envie seu arquivo</h1>
-        <form method="post" enctype="multipart/form-data" action="/upload">
-            <input type="file" name="file" required>
-            <input type="submit" value="Upload">
-        </form>
-    </div>
-</body>
-</html>
-'''  
+      return render_template('index.html')
 
 
 @app.route("/upload", methods=['POST'])
