3b4cf05a01393c741a1e38f22d26afa2c02fb8f4
Gileno Duarte <gileno.cr.duarte@gmail.com>
2024-06-03 21:02:09 -0300

    refactor: altera abordagem para load dos dados

--- app/etl/venda.py
+++ app/etl/venda.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, Boolean, Date, DECIMAL
 from flask import Flask,  request, render_template
 import pandas as pd
 import re
+import threading
 Base = declarative_base()
 formats_cpf= lambda cpf : f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}" 
 formats_cnpj = lambda cnpj : f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:]}"
@@ -22,23 +23,15 @@ class Vendas(Base):
     cnpj_valido= Column(Boolean, default=True)
     
     
-    def transformar_em_sublistas(self,lista_original):
-        lista_transformada = []
+    def make_list(self,lista_original):
+        changed_list = []
         for string in lista_original:
-            # Substituindo espaços em branco por vírgulas
             string=str(string).replace(',', '.')
-            print("Minha string: ", string)
-            string_com_virgulas = re.sub(r"\s+", ",", str(string))
-            #print(string_com_virgulas)
-            # Dividindo a string em sublistas
-            sublista = string_com_virgulas.replace("['","").replace("']", "").split(',')
-
-            # Adicionando a sublista à lista transformada
-            lista_transformada.append(sublista)
-            #print(lista_transformada)
-        return lista_transformada
+            string = re.sub(r"\s+", ",", str(string))
+            sublist = string.replace("['","").replace("']", "").split(',')
+            changed_list.append(sublist)
+        return changed_list
    
-
 
     @staticmethod
     def remove_empty_fields(data):
@@ -69,25 +62,33 @@ class Vendas(Base):
                 return False, formats_cnpj(cpfcnpj)
         
 
-    def load(self, file_path, conection):
-        '''with open(file_path, "r") as file:
-            lines= file.readlines()
-            for l in lines[1:]:
-                fields=self.remove_empty_fields(l)'''
-                
-        for bloco in pd.read_csv(file_path, chunksize=10000,sep='\t', iterator=True, header=None):
-            # Convertendo o bloco em um DataFrame do Pandas
-            #dados_df = bloco[0] 
-            result=self.transformar_em_sublistas(bloco.values.tolist()[1:])
-            for linha in list(result):
+    def save_object(self,df, conection):
+  
+        for line in list(df):
                 new_entry=Vendas()
-                new_entry.cpf_valido,new_entry.cpf=self.cpfcnpj_valid_format(linha[0])
-                new_entry.private=int(linha[1])
-                new_entry.incompleto=int(linha[2])
-                new_entry.data_ultima_compra=linha[3] if linha[3]!='NULL' else None
-                new_entry.ticket_medio=float(linha[4]) if linha[4]!='NULL' else 0.00
-                new_entry.ticket_medio_ultima_compra=float(linha[5]) if linha[5]!='NULL' else 0.00
-                new_entry.cnpj_valido, new_entry.loja_mais_frequente= self.cpfcnpj_valid_format(linha[6], 'j')
-                new_entry.cnpj_valido, new_entry.loja_da_ultima_compra= self.cpfcnpj_valid_format(linha[7], 'j')
+                new_entry.cpf_valido,new_entry.cpf=self.cpfcnpj_valid_format(line[0])
+                new_entry.private=int(line[1])
+                new_entry.incompleto=int(line[2])
+                new_entry.data_ultima_compra=line[3] if line[3]!='NULL' else None
+                new_entry.ticket_medio=float(line[4]) if line[4]!='NULL' else 0.00
+                new_entry.ticket_medio_ultima_compra=float(line[5]) if line[5]!='NULL' else 0.00
+                new_entry.cnpj_valido, new_entry.loja_mais_frequente= self.cpfcnpj_valid_format(line[6], 'j')
+                new_entry.cnpj_valido, new_entry.loja_da_ultima_compra= self.cpfcnpj_valid_format(line[7], 'j')
 
                 conection.save(new_entry)
+                new_entry = Vendas()
+
+
+                
+    def load(self, file_path, conection):  
+        threads = []      
+        for bloco in pd.read_csv(file_path, chunksize=3000,sep='\t',skiprows=1, iterator=True, header=None):
+            thread=threading.Thread(target=self.save_object, args=(self.make_list(bloco.values.tolist()), conection))
+            thread.start()
+            threads.append(thread)
+       
+        for thread in threads:
+            thread.join()
+
+
+       
